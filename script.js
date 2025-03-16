4b015ab32b60b6ca1a0a69df932c51ef9eb7e6fd
TOTEE45 <tata.bbeeee7@gmail.com>
2025-03-16 02:01:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.onkeydown = function(e) {
+    if (e.keyCode == 123 || 
+        (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || 
+                                     e.keyCode == 'J'.charCodeAt(0) || 
+                                     e.keyCode == 'C'.charCodeAt(0))) || 
+        (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))) {
+        return false;
+    }
+};
